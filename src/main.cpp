b503fe25753cac67d2c34740d4412e0a3d61a68a
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-01-27 19:01:48 +0000

    added bugs graphic

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <bn_core.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_sprite_ptr.h>
+
+#include "bn_sprite_items_bug_sprite.h"
 
 int main()
 {
     bn::core::init();
 
     bn::backdrop::set_color(bn::color(24, 31, 29));
+
+    bn::sprite_ptr ladybug = bn::sprite_items::bug_sprite.create_sprite();
 
     while (true)
     {
