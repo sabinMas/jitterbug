7a2cfbcc687d1980be530d4a11b53bf91f469101
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-01-29 11:10:09 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,47 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_sprite_ptr.h>
+#include <bn_random.h>
+#include <bn_vector.h>
+#include <bn_keypad.h>
 
 #include "bn_sprite_items_bug_sprite.h"
 
 int main()
 {
+    // Random number generator
+    bn::random rng = bn::random();
+
+    // Initialize Butano
     bn::core::init();
 
+    // Backdrop color (you can keep your original if you want)
     bn::backdrop::set_color(bn::color(24, 31, 29));
 
-    bn::sprite_ptr ladybug = bn::sprite_items::bug_sprite.create_sprite();
+    // Vector of ladybug sprites
+    bn::vector<bn::sprite_ptr, 50> bugs = {};
 
+    // Main game loop
     while (true)
     {
+        // Move all ladybug sprites randomly
+        for (bn::sprite_ptr &ladybug : bugs)
+        {
+            bn::fixed new_x = ladybug.x() + rng.get_fixed(-1, 1);
+            bn::fixed new_y = ladybug.y() + rng.get_fixed(-1, 1);
+
+            ladybug.set_x(new_x);
+            ladybug.set_y(new_y);
+        }
+
+        // If A is pressed, create a new ladybug and add it to the vector
+        if (bn::keypad::a_pressed())
+        {
+            bugs.push_back(bn::sprite_items::bug_sprite.create_sprite());
+        }
+
+        // Update RNG and frame
+        rng.update();
         bn::core::update();
     }
 }
